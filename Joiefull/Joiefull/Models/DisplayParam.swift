f6f816c5dff8a4183bc8a5e7af6d89d1ed2191c0
Bruno Evrard <bruno.evrard@gmail.com>
2025-02-28 16:31:24 +0100

    Suite commit précédents

---
+++ Joiefull/Joiefull/Models/DisplayParam.swift
@@ -0,0 +1,38 @@
+//
+//  ClotheRowParam.swift
+//  Joiefull
+//
+//  Created by Bruno Evrard on 26/02/2025.
+//
+
+
+import Foundation
+
+protocol DisplayParam {
+    
+    
+    // Common
+    static var pictureWidth: CGFloat { get }
+    static var pictureHeight: CGFloat { get }
+    static var fontSize: CGFloat { get }
+    static var starWidth: CGFloat { get }
+    static var starHeight: CGFloat { get }
+    
+    static var notationWidth: CGFloat { get }
+    static var notationHeight: CGFloat { get }
+    static var fontSizeNotation: CGFloat { get }
+    static var heartWidth: CGFloat { get }
+    static var heartHeight: CGFloat { get }
+    
+    // Row
+    static var rowHeight: CGFloat { get }
+    
+    // Detail
+    static var fontSizeDescription: CGFloat { get }
+    static var starNotationWidth: CGFloat { get }
+    static var starNotationHeight: CGFloat { get }
+    
+}
+
+
+
